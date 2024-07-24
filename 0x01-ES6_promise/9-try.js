097c1e9239c3f01c67a8776a56d9b3d0a0630880
4imaN <like93860@gmail.com>
2024-07-24 13:14:31 +0300

    check if there is deleted file

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,14 +1,14 @@
-export default function guardrail(mathFunction){
+export default function guardrail(mathFunction) {
     const queue = [];
     let res;
     try {
-        res = mathFunction();
-    } catch(error) {
-        res = `Error: ${error.message}`;
+      res = mathFunction();
+    } catch (error) {
+      res = `Error: ${error.message}`;
     }
     queue.push(res);
-    queue.push(guardrail);
+    queue.push('Guardrail was processed');
+  
     return queue;
-
-        
-}
+  }
+  

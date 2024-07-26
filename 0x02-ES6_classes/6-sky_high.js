1eee12478a6233623830aefdab313c052982af96
4imaN <like93860@gmail.com>
2024-07-26 05:03:56 +0300

    check if there is deleted file

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,21 +3,21 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this.floors = floors;
+    this._floors = floors; 
   }
 
   get floors() {
-    return this._floors;
+    return this._floors; 
   }
 
   set floors(newFloors) {
     if (typeof newFloors !== 'number') {
-      throw TypeError('Floors must be a number');
+      throw new TypeError('Floors must be a number');
     }
-    this._floors = newFloors;
+    this._floors = newFloors; 
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors`;
+    return `Evacuate slowly the ${this._floors} floors`; 
   }
 }

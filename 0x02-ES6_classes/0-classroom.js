bc837bf18ece5598d661644e1eb8ba0078930c33
4imaN <like93860@gmail.com>
2024-07-26 04:47:18 +0300

    check if there is deleted file

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,7 +1,7 @@
 export default class Classroom {
-  constructor(maxstudentsSize) {
-    this._maxStudentsSize = maxstudentsSize;
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
     
   }
 }
-        
+      

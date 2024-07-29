d8b804e6cf112b6f7a88cac3d17a7f2a619d9803
4imaN <like93860@gmail.com>
2024-07-29 20:29:28 +0300

    check if there is deleted file

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,22 @@
+function getListStudents() {
+  return [
+    {
+      id: 1,
+      firstName: 'John',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location:'San Francisco',
+    },
+    {
+      id: 3,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    },]
+}
+
+const students = getListStudents()
+
+console.log(students)
---
+++ 0x03-ES6_data_manipulation/README.md
@@ -0,0 +1,4 @@
+0x03 ES6 Data Manipulation
+=========================
+
+In this chapter we will learn how to use the new features of ES6 to manipulate data.

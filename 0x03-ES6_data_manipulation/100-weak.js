00ea69c8e924607f23f5fad7cfae8e12b8d45e95
4imaN <like93860@gmail.com>
2024-07-30 21:53:03 +0300

    check if there is deleted file

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,18 +1,23 @@
-export default function createWeakMap() {
-    return new WeakMap();
-}
-const weakMap = createWeakMap();
+    export const weakMap = new WeakMap();
 
-export default function queryAPI(endpoint) {
-  if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 1);
-  } else {
-    const count = weakMap.get(endpoint);
-    if (count >= 5) {
-      throw new Error('Endpoint load is high');
-    } else {
-      weakMap.set(endpoint, count + 1);
+    export function queryAPI(endpoint) {
+      if (!weakMap.get(endpoint)) weakMap.set(endpoint, 1);
+      else if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
+      else weakMap.set(endpoint, weakMap.get(endpoint) + 1);
     }
-  }
-}
+    const weakMap = new WeakMap();
 
+    function queryAPI(endpoint) {
+      if (!weakMap.has(endpoint)) {
+        weakMap.set(endpoint, 1);
+      } else {
+        const count = weakMap.get(endpoint);
+        if (count >= 5) {
+          throw new Error('Endpoint load is high');
+        } else {
+          weakMap.set(endpoint, count + 1);
+        }
+      }
+    }
+    
+    export { weakMap, queryAPI };

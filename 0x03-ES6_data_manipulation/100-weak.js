19a8602734539a7f5ac99b76b07258004912f972
4imaN <like93860@gmail.com>
2024-07-30 21:31:43 +0300

    check if there is deleted file

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,10 +1,7 @@
-export default function updateUniqueItems(map) {
-  if (!(map instanceof Map)) {
-    throw new Error('Cannot process');
-  }
-  for (const k of map.keys()) {
-    if (map.get(k) === 1) {
-      map.set(k, 100);
-    }
-  }
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  if (!weakMap.get(endpoint)) weakMap.set(endpoint, 1);
+  else if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
+  else weakMap.set(endpoint, weakMap.get(endpoint) + 1);
 }

82782f9f32a4e34621c34f8bbfc654dc3b6e0b70
4imaN <like93860@gmail.com>
2024-07-30 21:57:18 +0300

    check if there is deleted file

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position >= length) return new Error('Position outside range');
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
 
   const buffer = new ArrayBuffer(length);
-  const int8view = new Int8Array(buffer);
-  int8view[position] = value;
-
-  return buffer;
+  const view = new DataView(buffer);
+  view.setInt8(position, value);
+  return view;
 }
-  

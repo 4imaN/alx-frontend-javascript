19619ddbdb4d01ad385346e2b16eb83e88a44b3b
4imaN <like93860@gmail.com>
2024-08-01 18:07:44 +0300

    task 0

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -25,3 +25,21 @@ studentList.push(student1, student2);
 studentList.forEach((student) =>
   console.log(`${student.firstName} - ${student.location}`)
 );
+const studentsList: Student[] = [student1, student2];
+
+const table = document.createElement('table');
+
+studentsList.forEach((student) => {
+  const row = document.createElement('tr');
+  const nameCell = document.createElement('td');
+  const locationCell = document.createElement('td');
+
+  nameCell.textContent = student.firstName;
+  locationCell.textContent = student.location;
+
+  row.appendChild(nameCell);
+  row.appendChild(locationCell);
+  table.appendChild(row);
+});
+
+document.body.appendChild(table);
